0086dc8dc7f07741f07c9705286104706ce3792c
matti <matti@L-PORTATILE.station>
2022-03-11 12:33:28 +0100

    aggiunta esercizio 1 paw

---
+++ 00_PAWeb/test/it/unibo/paw/YourTestHere.java
@@ -0,0 +1,15 @@
+package it.unibo.paw;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class YourTestHere {
+
+	@Test
+	public void doSomething() {
+
+		fail("Not yet implemented");
+	}
+
+}
