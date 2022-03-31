ca224cf7ec0914220d9426333be2f42ab27a309e
matti <matti@L-PORTATILE.station>
2022-03-31 17:34:18 +0200

    aggiunta classi DAO per entita di mapping

---
+++ 05_PAWeb/src/it/unibo/paw/dao/CoursesStudentsMappingDTO.java
@@ -0,0 +1,19 @@
+package it.unibo.paw.dao;
+
+public class CoursesStudentsMappingDTO {
+	private int CourseId;
+	private int StudentId;
+	public int getCourseId() {
+		return CourseId;
+	}
+	public void setCourseId(int courseId) {
+		CourseId = courseId;
+	}
+	public int getStudentId() {
+		return StudentId;
+	}
+	public void setStudentId(int studentId) {
+		StudentId = studentId;
+	}
+
+}
