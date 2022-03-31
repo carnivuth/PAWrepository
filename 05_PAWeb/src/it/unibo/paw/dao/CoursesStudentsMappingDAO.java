a4cec4a765f1cc56f90d14e8818f77f0e1ec72d7
matti <matti@L-PORTATILE.station>
2022-03-31 17:34:36 +0200

    aggiunta classi DAo

--- 05_PAWeb/src/it/unibo/paw/dao/CoursesStudentsMappingDAO.java
+++ 05_PAWeb/src/it/unibo/paw/dao/CoursesStudentsMappingDAO.java
@@ -1,5 +1,16 @@
 package it.unibo.paw.dao;
 
 public interface CoursesStudentsMappingDAO {
+	public void create(CoursesStudentsMappingDTO course);
+
+	public CoursesStudentsMappingDTO read(int code);
+
+	public boolean update(CoursesStudentsMappingDTO course);
+
+	public boolean delete(int code);
+	
+	public boolean createTable();
+
+	public boolean dropTable();
 
 }
