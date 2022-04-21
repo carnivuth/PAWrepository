deabafc78680ecf3c86dfd447d3b1afbae97d0fd
matti <matti@L-PORTATILE.homenet.telecomitalia.it>
2022-04-21 10:31:51 +0200

    aggiunta classe course

---
+++ 07_PAWeb/src/it/unibo/paw/hibernate/Course.java
@@ -0,0 +1,23 @@
+package it.unibo.paw.hibernate;
+
+public class Course {
+	private int id;
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Course() {
+		
+	}
+	
+
+}
